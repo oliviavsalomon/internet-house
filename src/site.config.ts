c1d5d0824c23f2f253cac9b682554b22bf84ed7a
Olivia Salomon <oliviavsalomon@gmail.com>
2024-02-16 13:37:32 -0500

    minor configurations 1-3

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Olivia Salomon",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Astro Theme Cactus",
 	// Meta property used as the default description meta property
@@ -22,7 +22,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	webmentions: {
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		link: "",
 	},
 };
 
